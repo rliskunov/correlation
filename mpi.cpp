949473d8c5d9af16209bc2779aac381e63098970
rliskunov <liskunov.roma@yandex.ru>
2021-12-14 16:01:38 +0300

    fix

--- mpi.cpp
+++ mpi.cpp
@@ -58,7 +58,7 @@ int main(int argc, char **argv) {
         if (arrY[i] < averageY) arrY_part1[i] = arrY[i];
         else arrY_part2[i] = arrY[i];
 
-    wtime = MPI_Wtime();
+    double wtime = MPI_Wtime();
 
     double partial_coefficient = 0;
     double x_amount = 0, y_amount = 0, xy_amount = 0;
