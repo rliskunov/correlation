09e9de0efb47cd9aa14396ce24402b7e278ad30d
rliskunov <liskunov.roma@yandex.ru>
2021-12-30 14:39:30 +0300

    WIP

--- mpi.cpp
+++ mpi.cpp
@@ -132,9 +132,6 @@ int main(int argc, char **argv) {
         MPI_Reduce(&y_square_amount, &total_y_square_amount, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
     }
 
-    delete[] arrX;
-    delete[] arrY;
-
     MPI_Finalize();
     return 0;
 }
